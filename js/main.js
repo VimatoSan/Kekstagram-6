fc1976d4200eafd5724a9c597b2c73323ec26d16
Vimato <monst3k003@gmail.com>
2025-12-01 23:50:36 +0700

    Task 5.8

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ function main() {
   const commentGenerator = createCommentGenerator();
   const postGenerator = createPostGenerator(commentGenerator);
 
-  const size = 25;
+  const size = 25 ;
   const posts = [];
   for (let i = 1; i <= size; i++) {
     posts.push(postGenerator());
